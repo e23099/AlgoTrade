e77e7be1d061025d62d83f330fefe2cac7ec3d4e
e23099 <e23099@gmail.com>
2018-09-20 13:23:47 +0800

    修改口數、多空、"type"等變數名稱

--- CTrade.py
+++ CTrade.py
@@ -1,6 +1,6 @@
 class CTrade():
+    direction = 0
     pos = 0
-    volume = 0
     inPos = []
     outPos = []
     inTime = []
@@ -9,70 +9,41 @@ class CTrade():
     outPrice = []
     Note = []
 
-    def __init__(self, position, n, price, type = "start"):
-        self.pos = position
-        self.volume = n
+    def __init__(self, dir, n, price, reason = "start"):
+        self.direction = dir
+        self.pos = n
         self.inPos.append(n)
         self.inPrice.append(price)
-        self.Note.append(type)
+        self.Note.append(reason)
 
-    def add(self, time, n, price, type = "add contract"):
+    def add(self, time, n, price, reason = "add contract"):
         self.inTime.append(time)
         self.inPrice.append(price)
-        self.Note.append(type)
+        self.Note.append(reason)
         self.inPos.append(n)
-        self.volume += n
+        self.pos += n
 
-    def stop(self, time, price, type = "stop"):
+    def stop(self, time, price, reason = "stop"):
         self.outTime.append(time)
         self.outPrice.append(price)
-        self.Note.append(type)
-        self.outPos.append(self.volume)
-        self.volume = 0
+        self.Note.append(reason)
+        self.outPos.append(self.pos)
+        self.pos = 0
 
-    def limit(self, time, price, type = "limit"):
+    def limit(self, time, price, reason = "limit"):
         self.outTime.append(time)
         self.outPrice.append(price)
-        self.Note.append(type)
-        self.outPos.append(self.volume)
-        self.volume = 0
+        self.Note.append(reason)
+        self.outPos.append(self.pos)
+        self.pos = 0
 
-    def cut(self, time, n, price, type = "cut"):
+    def cut(self, time, n, price, reason = "cut"):
         self.outTime.append(time)
         self.outPrice.append(price)
-        self.Note.append(type)
+        self.Note.append(reason)
         self.outPos.append(n)
-        self.volume -= n
+        self.pos -= n
 
     def end(self):
         # write trade information into a table
         pass
-
-trade = CTrade(-1, 3, 10666)
-print("Current Volume:", trade.volume, ", dir:", trade.pos)
-trade.add('08:50', 2, 10633)
-print("Current Volume:", trade.volume, ", dir:", trade.pos)
-trade.cut('12:34', 1, 10600)
-print("Current Volume:", trade.volume, ", dir:", trade.pos)
-trade.stop('13:45', 10638)
-print("Current Volume:", trade.volume, ", dir:", trade.pos)
-
-print("inPrice list:")
-for price in trade.inPrice:
-    print("  ", price)
-
-print("outPrice list:")
-for price in trade.outPrice:
-    print("  ", price)
-
-print("note list:")
-for note in trade.Note:
-    print("  ", note)
-
-print("inPos list:")
-for n in trade.inPos:
-    print("  ", n)
-
-print("outPos list:")
-for n in trade.outPos:
-    print("  ", n)
