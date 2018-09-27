6c77c0bea4aec003f97803b39837b59dbe05901d
e23099 <e23099@gmail.com>
2018-09-27 22:33:07 +0800

    新增損益出場元件，可用於停損、停利小幫手

---
+++ Strategy_element.py
@@ -0,0 +1,24 @@
+# 損益出場
+
+def byProfit(price, x, cost, stop_limit):
+    """
+    根據損益點數，出清所有部位。
+
+    Example:
+
+        byProfit(20, InPrice.max(), -1)    以成本最高者計算停損點
+        byProfit(20, get_open_price(), -1) 以開盤價計算停損點
+        byProfit(100, InPrice[-1] , 1)     以最新單計算停利點
+
+    :param x: 損益點數
+    :param cost: 成本計價方式(最新價、最高價、平均價、特定價格)
+    :param stop_limit: 停損或停利(1/-1)
+    :return: True/False 或應出場價格
+    """
+
+    profit = (price - cost) * Trade.dir
+
+    if profit * stop_limit >= x:
+        return(True) # or return(cost + x * stop_limit * Trade.dir)
+    else:
+        return(False)
