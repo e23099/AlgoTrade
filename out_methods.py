024e036b80aab9508bc2f48ed5246852f48b7fb2
tt160702 <43596694+tt160702@users.noreply.github.com>
2018-10-03 15:56:19 +0800

    Update out_methods.py

--- out_methods.py
+++ out_methods.py
@@ -27,9 +27,9 @@ def byProfit(price, x, cost, stop_limit):
     
 def byTime(day=0,hour=0,minute=0,second=0,spe_date_or_weekday,typee=1,in_time):
     """
-    時間格式: 年(西元)月日時分秒 (特定日期或星期使用)
-             ex. 20181002180605  (2018/10/02,18:06:05) (表特定日期)
-             ex. 5  (表星期)
+    時間格式: 年(西元)/月/日/星期/時/分/秒 (特定日期或星期使用)
+             ex. 2018/10/02/0/18/06/05             
+             ex. 0000/00/00/2/00/00/00  (表星期)
     根據時間，出清所有部位。
     
     :param day: 平倉距離進場的時長天數
@@ -46,4 +46,56 @@ def byTime(day=0,hour=0,minute=0,second=0,spe_date_or_weekday,typee=1,in_time):
     
     
     if typee == 1:
-        
+        in_Time=in_time.split("/")
+        int_in_time=[]
+        for row in in_Time:
+            k=int(row)
+            int_in_time.append(k)
+        out_time=[]
+        out_time.append(int_in_time[0])
+        out_time.append(int_in_time[1])
+        out_time.append(int_in_time[2]+day)
+        out_time.append(0)
+        out_time.append(int_in_time[4]+hour)
+        out_time.append(int_in_time[5]+minute)
+        out_time.append(int_in_time[6]+second)
+        if out_time[6] >= 60:
+            out_time[6] = out_time[6] - 60
+            out_time[5] = out_time[5] + 1
+        if out_time[5] >= 60:
+            out_time[5] = out_time[6] - 60
+            out_time[4] = out_time[5] + 1
+        if out_time[4] >= 24:
+            out_time[4] = out_time[6] - 24
+            out_time[2] = out_time[5] + 1
+        if out_time[2] >= 30 and (out_time[1] in [2,4,6,9,11] ) :
+            out_time[2] = out_time[2] - 30
+            out_time[1] = out_time[1] + 1
+        elif out_time[2] >= 31 and (out_time[1] in [1,3,5,7,8,10,12] ) :
+            out_time[2] = out_time[2] - 31
+            out_time[1] = out_time[1] + 1
+        if out_time[1] >= 12:
+            out_time[1] = out_time[1] - 12
+            out_time[0] = out_time[0] + 1
+        time=time.split("/")
+        int_time=[]
+        for row in time:
+            k=int(row)
+            int_time.append(k)
+        if int_time == out_time :
+            return True
+    if typee == 2:
+        spe_date_or_weekday=spe_date_or_weekday.split("/")
+        int_spe_date_or_weekday=[]
+        for row in spe_date_or_weekday:
+            k=int(row)
+            int_spe_date_or_weekday.append(k)
+        time=time.split("/")
+        int_time=[]
+        for row in time:
+            k=int(row)
+            int_time.append(k)
+        if int_spe_date_or_weekday == int_time :
+            return True
+        if int_spe_data_or_weekday[3] == int_time[3] :
+            return True
